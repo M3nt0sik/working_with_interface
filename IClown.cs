1c390dd3b69d08e858e9ac33143513ae0f3d7e5b
Kamil <mentos.96@o2.pl>
2023-06-09 22:06:30 +0200

    Write interface IClown

---
+++ IClown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace working_with_interface
+{
+    internal interface IClown
+    {
+        string FunnyThingsIHave{ get; }
+        void Honk();
+    }
+}
