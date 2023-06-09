471c7bed55f3fa773b8fa3a233568bf29f158d6a
Kamil <mentos.96@o2.pl>
2023-06-09 22:34:00 +0200

    Write interfeace IScaryClown and class FunnyFunny included interface IClown

---
+++ FunyFuny.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace working_with_interface
+{
+    class FunyFuny:IClown
+    {
+        private string funnyThingsIHave="";
+        public string FunnyThingsIHave { get { return funnyThingsIHave; } }
+
+        FunyFuny(string _funnyThings)
+        {
+            funnyThingsIHave= _funnyThings;
+
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine($"Cześć dzieciaki! Mam: {FunnyThingsIHave}.");
+        }
+        
+}
---
+++ IScaryClown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace working_with_interface
+{
+    internal interface IScaryClown:IClown
+    {
+        String ScaryThinkIHave { get; }
+        void ScareLittleChildren();
+    }
+}
